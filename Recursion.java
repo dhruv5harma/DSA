9501c657dea6e03dcbbd5885a463ec1c424af621
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2022-07-27 07:55:54 +0530

    Added Taylor and Cosine Series Using Recursion
    1-Taylor Series
    2-Taylor Series Using Iteration
    3-Taylor Series using Homer's rule
    4-Cosine Series

--- Recursion.java
+++ Recursion.java
@@ -6,6 +6,9 @@ public class Recursion{
 		System.out.println(factorial(10));
 		System.out.println(sumOfDigits(1011));
 		System.out.println(powerOfNum(-2,12));
+		System.out.println(taylorSeries(1,10));
+		System.out.println(taylorSeriesUsingHomerRuleIterative(1,10));
+		System.out.println(taylorSeriesUsingHomerRuleRecursion(1,10));
 	}
 	
 	public static int sumOfNaturalNumber(int x){
@@ -38,6 +41,42 @@ public class Recursion{
 			return 1;
 		}else {
 			return x*powerOfNum(x,pow-1);
+		}
+	}
+	
+	private static int fact=1;
+	private static int pow=1;
+	
+	public static double taylorSeries(int e, int x){
+		if(x==0){
+			return 1;
+		} else {
+			double result = taylorSeries(e,x-1);
+			fact*=x;
+			pow*=e;
+			return result + (double)pow/fact;
+		}
+	}
+	
+	//Taylor series using homer's rule-Iterative Version
+	
+	private static double sumOfTaylor=1;
+	public static double taylorSeriesUsingHomerRuleIterative(int x, int n){
+		for(;n>0;n--){
+			sumOfTaylor=1+((double)x/n)*sumOfTaylor;
+		}
+		return sumOfTaylor;
+	}
+	
+	//Taylor series using homer's rule-Recursive Version
+	
+	private static double sumOfTaylorRecursion=1;
+	public static double taylorSeriesUsingHomerRuleRecursion(int x, int n){
+		if(n==0){
+			return sumOfTaylorRecursion;
+		} else {
+			sumOfTaylorRecursion=1+((double)x/n)*sumOfTaylorRecursion;
+			return taylorSeriesUsingHomerRuleRecursion(x,n-1);
 		}
 	}
 	
